489ad5138e68ced5332b2cc601498ba3e5f398ce
Aleksandra Sobanska <avriola@vp.pl>
2024-08-22 12:32:56 +0200

    security api

--- src/index.js
+++ src/index.js
@@ -7,33 +7,53 @@
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
-/**
- * @param {Request} request
- */
-const corsHeaders = {
+
+const allowedOrigins = [
+	"https://localhost:3000",
+	"https://unsplash-cloudflare-page.pages.dev/"
+]
+const corsHeaders = origin => ({
 	'Access-Control-Allow-Headers': "*",
 	'Access-Control-Allow-Methods': "POST",
-	'Access-Control-Allow-Origin': "*"
+	'Access-Control-Allow-Origin': origin
+})
+
+const checkOrigin = request => {
+	const origin = request.headers.get("Origin")
+	const foundOrigin = allowedOrigins.find(allowedOrigin => allowedOrigin.includes(origin))
+	return foundOrigin ? foundOrigin : allowedOrigins[0]
 }
 
 const getImages = async request => {
 	const CLIENT_ID = "sYCTLK04wMqwqi7XCWhsqu2YphveyN86l7jWnWPZJNU";
 	const { query } = await request.json();
-	const resp = await fetch(`https://api.unsplash.com/search/photos?query${query}`, {
-		headers: {
-			Authorization: `Client-ID ${CLIENT_ID}`
-		}
-	})
+
+	// Create a new Unsplash URL
+	const url = new URL("https://api.unsplash.com/search/photos")
+	// Set the client ID from a secret set via `wrangler secret`
+	url.searchParams.set("client_id", CLIENT_ID)
+	// Only return nine results for a nice grid layout
+	url.searchParams.set("per_page", 9)
+	// Set the query/keyword to the value from request body
+	url.searchParams.set("query", query)
+
+	// Make a request to Unsplash's API
+	const resp = await fetch(url)
+
 	const data = await resp.json()
 	const images = data.results.map(image => ({
 		id: image.id,
 		image: image.urls.small,
 		link: image.links.html,
 	}))
+
+	// Check that the request's origin is a valid origin, allowed to access this API
+	const allowedOrigin = checkOrigin(event.request)
+
 	return new Response(JSON.stringify(images), {
 		headers: {
 			'Content-type': 'application-json',
-			...corsHeaders
+			...corsHeaders(allowedOrigin)
 		}
 	})
 }
@@ -41,7 +61,8 @@ const getImages = async request => {
 export default {
 	async fetch(request, env, ctx) {
 		if (request.method === "OPTIONS") {
-			return new Response("OK", { headers: corsHeaders })
+			const allowedOrigin = checkOrigin(event.request)
+			return new Response("OK", { headers: corsHeaders(allowedOrigin) })
 		}
 		if (request.method === "POST") {
 			return getImages(request)
